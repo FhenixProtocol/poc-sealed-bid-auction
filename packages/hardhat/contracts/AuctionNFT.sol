072ec333101724e12d67d119b8696679fa3b8365
Alexandre Carvalheira <axxcar@gmail.com>
2025-12-29 19:37:25 -0300

    feat: add AuctionNFT ERC721 for demo

---
+++ packages/hardhat/contracts/AuctionNFT.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { ERC721URIStorage } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+/// @title AuctionNFT
+/// @notice A simple ERC721 for the sealed bid auction demo
+contract AuctionNFT is ERC721, ERC721URIStorage {
+    uint256 private _nextTokenId;
+
+    constructor() ERC721("Auction NFT", "ANFT") {}
+
+    /// @notice Mint a new NFT
+    /// @param to The address to mint to
+    /// @param uri The token URI (metadata)
+    /// @return tokenId The ID of the minted token
+    function mint(address to, string memory uri) external returns (uint256 tokenId) {
+        tokenId = _nextTokenId++;
+        _mint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // Required overrides
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
